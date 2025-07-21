2c1fe0a5c09b651a0f1f2063ab7c912776f1f326
Philip Krag Nielsen <philipkragnielsen@gmail.com>
2025-07-21 22:09:06 +0200

    Add Seesaw

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :description "Simple tool for cutting a map/image out into tiles written in Clojure."
   :url "https://github.com/KNPhilip/map-tile-cutter"
   :main map-tile-cutter.core
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [seesaw "1.5.0"]]
   :profiles {:dev {:dependencies [[speclj "3.3.2"]]}}
   :plugins [[speclj "3.3.2"]]
   :test-paths ["spec"])
--- src/map_tile_cutter/core.clj
+++ src/map_tile_cutter/core.clj
@@ -1,5 +1,25 @@
-(ns map-tile-cutter.core)
+(ns map-tile-cutter.core
+  (:use seesaw.core))
+
+(defn make-section [name id]
+  (let [stat-label (label name)
+        stat-data (label :text "" :id id :size [100 :by 20])
+        stat-panel (top-bottom-split stat-label stat-data)]
+    stat-panel))
 
 (defn -main
   [& args]
-  "Hello World")
+  (let [window (frame :title "Map Tile Cutter")
+        input-section (make-section "Input" :input-options)
+        cutting-options-section (make-section "Cutting Options" :cut-ops)
+        export-section (make-section "Export" :export-options)]
+
+    (config! window :content
+      (vertical-panel :items
+        (concat [input-section
+                 cutting-options-section
+                 export-section])))
+
+    (pack! window)
+    (show! window)
+    ) "Hello World")
