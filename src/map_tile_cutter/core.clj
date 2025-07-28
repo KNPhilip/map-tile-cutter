242df04d45ad529dab90f4f2281e0843e7ade173
Philip Krag Nielsen <philipkragnielsen@gmail.com>
2025-07-28 21:04:12 +0200

    Move Mainframe to Presenter

--- src/map_tile_cutter/core.clj
+++ src/map_tile_cutter/core.clj
@@ -1,32 +1,7 @@
 (ns map-tile-cutter.core
-  (:use seesaw.core))
-
-(defn make-section [name id]
-  (let [stat-label (label name)
-        stat-data (label :text "" :id id :size [100 :by 20])
-        stat-panel (top-bottom-split stat-label stat-data)]
-    stat-panel))
-
-(defn mainframe-content []
-  (let [input (make-section "Input" :input-options)
-        cutting-options (make-section "Cutting Options" :cut-ops)
-        export (make-section "Export" :export-options)]
-    (border-panel
-      :north input
-      :center cutting-options
-      :south export)))
-
-(defn make-mainframe []
-  (let [content (mainframe-content)]
-    (frame
-      :title "Map Tile Cutter"
-      :content content
-      :on-close :exit)))
+  (:require [map-tile-cutter.presenter :as presenter])
+  (:use [seesaw.core]))
 
 (defn -main []
   (native!)
-  (invoke-later
-    (-> (make-mainframe)
-        pack!
-        (config! :size [800 :by 400])
-        show!)))
+  (presenter/present-mainframe))
---
+++ src/map_tile_cutter/presenter.clj
@@ -0,0 +1,31 @@
+(ns map-tile-cutter.presenter
+  (:use [seesaw.core]))
+
+(defn make-section [name id]
+  (let [stat-label (label name)
+        stat-data (label :text "" :id id :size [100 :by 20])
+        stat-panel (top-bottom-split stat-label stat-data)]
+    stat-panel))
+
+(defn mainframe-content []
+  (let [input (make-section "Input" :input-options)
+        cutting-options (make-section "Cutting Options" :cut-ops)
+        export (make-section "Export" :export-options)]
+    (border-panel
+      :north input
+      :center cutting-options
+      :south export)))
+
+(defn make-mainframe []
+  (let [content (mainframe-content)]
+    (frame
+      :title "Map Tile Cutter"
+      :content content
+      :on-close :exit)))
+
+(defn present-mainframe []
+  (invoke-later
+    (-> (make-mainframe)
+        pack!
+        (config! :size [800 :by 400])
+        show!)))
