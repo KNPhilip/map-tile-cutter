f2680f396aa22c51c3e3077f0bdf61698cae41dd
Philip Krag Nielsen <philipkragnielsen@gmail.com>
2025-07-28 21:26:45 +0200

    Add Button & Format

--- src/map_tile_cutter/presenter.clj
+++ src/map_tile_cutter/presenter.clj
@@ -11,30 +11,34 @@
 (defn cutting-options-section []
   (vertical-panel
     :items [(horizontal-panel
-              :items [(label :text "Tile size:")
-                      (text)
-                      (label :text "Cuts:")
-                      (text)])
+              :items [(label    :text "Tile size:")
+                      (text     :text "256")
+                      (label    :text "Cuts:")
+                      (text     :text "1")])
             (horizontal-panel
-              :items [(label :text "Background color:")
-                      (text)
-                      (label :text "Extension:")
+              :items [(label    :text "Background color:")
+                      (text     :text "#00FFFFF")
+                      (label    :text "Extension:")
                       (combobox :model ["PNG"
                                         "JPEG"
                                         "GIF"])])
             ]))
 
+(defn submit-button []
+  (horizontal-panel
+    :items [(button :text "Submit")]))
+
 (defn export-section []
   (vertical-panel
     :items [(horizontal-panel
-              :items [(label :text "Format:")
+              :items [(label    :text "Format:")
                       (combobox :model ["z/x_y.png"
                                         "z_x_y.png"
                                         "z/x/y.png"])])
             (horizontal-panel
               :items [(label :text "Export path:")
                       (text)])
-           ]))
+            (submit-button)]))
 
 (defn mainframe-content []
   (let [input (input-section)
