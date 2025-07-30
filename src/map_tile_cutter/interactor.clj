38474e567d31263a31e5fdcc9cc187776eb3b405
Philip Krag Nielsen <philipkragnielsen@gmail.com>
2025-07-30 11:48:08 +0200

    Support Non-Square Input Images

--- src/map_tile_cutter/interactor.clj
+++ src/map_tile_cutter/interactor.clj
@@ -30,6 +30,14 @@
       (.dispose g))
     scaled-img))
 
+(defn create-bg-img [width height bg-color]
+  (let [bg-img (java.awt.image.BufferedImage. width height java.awt.image.BufferedImage/TYPE_INT_RGB)
+        g (.createGraphics bg-img)]
+    (.setColor g (java.awt.Color. (Integer/parseInt (str/replace bg-color "#" "") 16)))
+    (.fillRect g 0 0 width height)
+    (.dispose g)
+    bg-img))
+
 (defn cut-tiles [rows cols tile-size resized-img extension exp-path cuts format]
   (dotimes [row rows]
     (dotimes [col cols]
@@ -39,10 +47,26 @@
             relative-path (format-path format cuts col row extension)
             output-file (io/file exp-path relative-path)]
         (io/make-parents output-file)
-        (ImageIO/write sub-img extension output-file)))))
+        (ImageIO/write sub-img extension output-file))))
+  (alert (str "All tiles were cut out and exported to \"" exp-path "\"")))
 
 (defn cut-non-square-image [img tile-size cuts bg-color extension format exp-path]
-  "Not Square!")
+  (let [width (.getWidth img)
+        height (.getHeight img)
+        max-dimension (max width height)
+        new-dimension (int (Math/ceil (/ max-dimension tile-size)))
+        new-width (* new-dimension tile-size)
+        new-height (* new-dimension tile-size)
+        bg-img (create-bg-img new-width new-height bg-color)
+        g (.createGraphics bg-img)]
+    (.drawImage g img 0 0 width height nil)
+    (.dispose g)
+    (let [resized-img (resize-img bg-img new-width new-height)
+          num-tiles (int (Math/pow 4 cuts))
+          rows (int (Math/sqrt num-tiles))
+          cols (int (Math/sqrt num-tiles))]
+      (cut-tiles rows cols tile-size resized-img extension exp-path cuts format)
+      "Not Square!")))
 
 (defn cut-square-image [img tile-size cuts extension format exp-path]
   (let [width (int (* (Math/pow 2 cuts) tile-size))
